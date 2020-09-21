421debd5b3c15642cf7fe8d4dc1ed051b5008f67
abdiwali96 <Elthorne2greenford>
2020-09-21 09:47:37 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Abdiwali Abdi on 21/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
